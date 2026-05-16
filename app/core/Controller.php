27ec4dc68a4ac580a813111b6db09024c592dbbb
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-05-17 07:03:34 +0800

    Add Controller core: admin(), app(), client(), auth() view render helpers

---
+++ app/core/Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+class Controller
+{
+    public function client($view, $data = [])
+    {
+        extract($data);
+        include 'app/views/layouts/client/header.php';
+        include "app/views/$view.php";
+        include 'app/views/layouts/client/footer.php';
+    }
+
+    public function admin($view, $data = [])
+    {
+        extract($data);
+        include 'app/views/layouts/admin/header.php';
+        include "app/views/$view.php";
+        include 'app/views/layouts/admin/footer.php';
+    }
+
+    public function app($view, $data = [])
+    {
+        extract($data);
+        include 'app/views/layouts/app/header.php';
+        include "app/views/$view.php";
+        include 'app/views/layouts/app/footer.php';
+    }
+
+    public function auth($view, $data = [])
+    {
+        extract($data);
+        include 'app/views/layouts/auth/header.php';
+        include "app/views/$view.php";
+        include 'app/views/layouts/auth/footer.php';
+    }
+}
