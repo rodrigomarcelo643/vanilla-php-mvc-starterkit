9bca2407635b4d46781d9dafb4c594fcd5e60073
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-05-17 07:04:33 +0800

    Add Session core: set, get, destroy, and flash message session helpers

---
+++ app/core/Session.php
@@ -0,0 +1,19 @@
+<?php
+
+class Session
+{
+    public static function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key)
+    {
+        return $_SESSION[$key] ?? null;
+    }
+
+    public static function destroy()
+    {
+        session_destroy();
+    }
+}
