69d0f95393de391b8e38a70cfba7cbac6e0ee176
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2026-05-27 18:32:19 +0800

    feat(routes): register web routes for terms and privacy pages

--- routes/web/client/pages.php
+++ routes/web/client/pages.php
@@ -6,3 +6,5 @@ Router::get('about',   ['HomeController',    'about']);
 Router::get('docs',    ['HomeController',    'docs']);
 Router::get('blog',    ['HomeController',    'blog']);
 Router::get('profile', ['ProfileController', 'index']);
+Router::get('terms',   ['LegalController',   'terms']);
+Router::get('privacy', ['LegalController',   'privacy']);
